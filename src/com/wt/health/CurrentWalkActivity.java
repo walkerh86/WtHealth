54cf49dca4badb36308dfe5667ef16e212b337c4
hcj <hcj@hcj.com>
2017-03-25 23:45:28 +0800

    update

---
+++ src/com/wt/health/CurrentWalkActivity.java
@@ -0,0 +1,396 @@
+/*
+ *  Pedometer - Android App
+ *  Copyright (C) 2009 Levente Bagi
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.wt.health;
+
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class CurrentWalkActivity extends Activity {
+	private static final String TAG = "hcj.CurrentWalkActivity";
+    private SharedPreferences mSettings;
+    private PedometerSettings mPedometerSettings;
+    private Utils mUtils;
+    
+    private TextView mStepValueView;
+    private TextView mPaceValueView;
+    private TextView mDistanceValueView;
+    private TextView mSpeedValueView;
+    private TextView mCaloriesValueView;
+    TextView mDesiredPaceView;
+    private int mStepValue;
+    private int mPaceValue;
+    private float mDistanceValue;
+    private float mSpeedValue;
+    private int mCaloriesValue;
+    private float mDesiredPaceOrSpeed;
+    private int mMaintain;
+    private boolean mIsMetric;
+    private float mMaintainInc;
+    private boolean mQuitting = false; // Set when user selected Quit from menu, can be used by onPause, onStop, onDestroy
+    
+    private Button mStartBtn;
+    private Button mStopBtn;
+    private Button mRestartBtn;
+    
+    /**
+     * True, when service is running.
+     */
+    private boolean mIsRunning;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "[ACTIVITY] onCreate");
+        super.onCreate(savedInstanceState);
+        
+        mStepValue = 0;
+        mPaceValue = 0;
+        
+        setContentView(R.layout.health_layout);
+        
+        mStepValueView     = (TextView) findViewById(R.id.steps_value);
+        //mPaceValueView     = (TextView) findViewById(R.id.pace_value);
+        mDistanceValueView = (TextView) findViewById(R.id.distance_value);
+        //mSpeedValueView    = (TextView) findViewById(R.id.speed_value);
+        //mCaloriesValueView = (TextView) findViewById(R.id.calories_value);
+        //mDesiredPaceView   = (TextView) findViewById(R.id.desired_pace_value);
+        mStepValueView.setText("0");
+        mDistanceValueView.setText("0");
+        
+        mStartBtn = (Button)findViewById(R.id.control_start);
+        mStartBtn.setOnClickListener(mOnClickListener);
+        mStopBtn = (Button)findViewById(R.id.control_stop);
+        mStopBtn.setOnClickListener(mOnClickListener);
+        mRestartBtn = (Button)findViewById(R.id.control_restart);
+        mRestartBtn.setOnClickListener(mOnClickListener);
+        
+        mUtils = Utils.getInstance();
+    }
+    
+    private void updateBtn(){
+    	if(mIsRunning){
+    		mStopBtn.setVisibility(View.VISIBLE);
+    		mStartBtn.setVisibility(View.GONE);
+    	}else{
+    		mStopBtn.setVisibility(View.GONE);
+    		mStartBtn.setVisibility(View.VISIBLE);
+    	}
+    }
+    
+    private View.OnClickListener mOnClickListener = new View.OnClickListener(){
+    	@Override
+    	public void onClick(View view){
+    		if(view == mStartBtn){
+    			startStepService();
+                bindStepService();
+    		}else if(view == mStopBtn){
+    			unbindStepService();
+                stopStepService();
+    		}
+    		updateBtn();
+    	}
+    };
+    
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "[ACTIVITY] onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "[ACTIVITY] onResume");
+        super.onResume();
+        
+        mSettings = PreferenceManager.getDefaultSharedPreferences(this);
+        mPedometerSettings = new PedometerSettings(mSettings);
+        
+        mUtils.setSpeak(mSettings.getBoolean("speak", false));
+        
+        // Read from preferences if the service was running on the last onPause
+        mIsRunning = mPedometerSettings.isServiceRunning();
+        
+        updateBtn();
+        
+        // Start the service if this is considered to be an application start (last onPause was long ago)
+        /*
+        if (!mIsRunning && mPedometerSettings.isNewStart()) {
+            startStepService();
+            bindStepService();
+        }
+        else if (mIsRunning) {
+            bindStepService();
+        }
+        */
+        mPedometerSettings.clearServiceRunning();        
+    }
+    
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "[ACTIVITY] onPause");
+        if (mIsRunning) {
+            unbindStepService();
+        }
+        if (mQuitting) {
+            mPedometerSettings.saveServiceRunningWithNullTimestamp(mIsRunning);
+        }
+        else {
+            mPedometerSettings.saveServiceRunningWithTimestamp(mIsRunning);
+        }
+
+        super.onPause();
+        savePaceSetting();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "[ACTIVITY] onStop");
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        Log.i(TAG, "[ACTIVITY] onDestroy");
+        super.onDestroy();
+    }
+    
+    protected void onRestart() {
+        Log.i(TAG, "[ACTIVITY] onRestart");
+        super.onDestroy();
+    }
+
+    private void setDesiredPaceOrSpeed(float desiredPaceOrSpeed) {
+        if (mService != null) {
+            if (mMaintain == PedometerSettings.M_PACE) {
+                mService.setDesiredPace((int)desiredPaceOrSpeed);
+            }
+            else
+            if (mMaintain == PedometerSettings.M_SPEED) {
+                mService.setDesiredSpeed(desiredPaceOrSpeed);
+            }
+        }
+    }
+    
+    private void savePaceSetting() {
+        mPedometerSettings.savePaceOrSpeedSetting(mMaintain, mDesiredPaceOrSpeed);
+    }
+
+    private StepService mService;
+    
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = ((StepService.StepBinder)service).getService();
+            android.util.Log.i(TAG, "onServiceConnected mService="+mService);
+
+            mService.registerCallback(mCallback);
+            mService.reloadSettings();
+            
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            mService = null;
+        }
+    };
+    
+
+    private void startStepService() {
+        if (! mIsRunning) {
+            Log.i(TAG, "[SERVICE] Start");
+            mIsRunning = true;
+            startService(new Intent(CurrentWalkActivity.this,
+                    StepService.class));
+        }
+    }
+    
+    private void bindStepService() {
+        Log.i(TAG, "[SERVICE] Bind");
+        bindService(new Intent(CurrentWalkActivity.this, 
+                StepService.class), mConnection, Context.BIND_AUTO_CREATE + Context.BIND_DEBUG_UNBIND);
+    }
+
+    private void unbindStepService() {
+        Log.i(TAG, "[SERVICE] Unbind");
+        if(mService == null){
+        	return;
+        }
+        unbindService(mConnection);
+    }
+    
+    private void stopStepService() {
+        Log.i(TAG, "[SERVICE] Stop");
+        if (mService != null) {
+            Log.i(TAG, "[SERVICE] stopService");
+            stopService(new Intent(CurrentWalkActivity.this,
+                  StepService.class));
+        }
+        mIsRunning = false;
+    }
+    
+    private void resetValues(boolean updateDisplay) {
+        if (mService != null && mIsRunning) {
+            mService.resetValues();                    
+        }
+        else {
+            mStepValueView.setText("0");
+            mPaceValueView.setText("0");
+            mDistanceValueView.setText("0");
+            mSpeedValueView.setText("0");
+            mCaloriesValueView.setText("0");
+            SharedPreferences state = getSharedPreferences("state", 0);
+            SharedPreferences.Editor stateEditor = state.edit();
+            if (updateDisplay) {
+                stateEditor.putInt("steps", 0);
+                stateEditor.putInt("pace", 0);
+                stateEditor.putFloat("distance", 0);
+                stateEditor.putFloat("speed", 0);
+                stateEditor.putFloat("calories", 0);
+                stateEditor.commit();
+            }
+        }
+    }
+
+    private static final int MENU_SETTINGS = 8;
+    private static final int MENU_QUIT     = 9;
+
+    private static final int MENU_PAUSE = 1;
+    private static final int MENU_RESUME = 2;
+    private static final int MENU_RESET = 3;
+    
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_PAUSE:
+                unbindStepService();
+                stopStepService();
+                return true;
+            case MENU_RESUME:
+                startStepService();
+                bindStepService();
+                return true;
+            case MENU_RESET:
+                resetValues(true);
+                return true;
+            case MENU_QUIT:
+                resetValues(false);
+                unbindStepService();
+                stopStepService();
+                mQuitting = true;
+                finish();
+                return true;
+        }
+        return false;
+    }
+ 
+    // TODO: unite all into 1 type of message
+    private StepService.ICallback mCallback = new StepService.ICallback() {
+        public void stepsChanged(int value) {
+            mHandler.sendMessage(mHandler.obtainMessage(STEPS_MSG, value, 0));
+        }
+        public void paceChanged(int value) {
+            //mHandler.sendMessage(mHandler.obtainMessage(PACE_MSG, value, 0));
+        }
+        public void distanceChanged(float value) {
+            mHandler.sendMessage(mHandler.obtainMessage(DISTANCE_MSG, (int)(value*1000), 0));
+        }
+        public void speedChanged(float value) {
+            //mHandler.sendMessage(mHandler.obtainMessage(SPEED_MSG, (int)(value*1000), 0));
+        }
+        public void caloriesChanged(float value) {
+            //mHandler.sendMessage(mHandler.obtainMessage(CALORIES_MSG, (int)(value), 0));
+        }
+    };
+    
+    private static final int STEPS_MSG = 1;
+    private static final int PACE_MSG = 2;
+    private static final int DISTANCE_MSG = 3;
+    private static final int SPEED_MSG = 4;
+    private static final int CALORIES_MSG = 5;
+    
+    private Handler mHandler = new Handler() {
+        @Override public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STEPS_MSG:
+                    mStepValue = (int)msg.arg1;
+                    mStepValueView.setText("" + mStepValue);
+                    break;
+                case PACE_MSG:
+                    mPaceValue = msg.arg1;
+                    if (mPaceValue <= 0) { 
+                        mPaceValueView.setText("0");
+                    }
+                    else {
+                        mPaceValueView.setText("" + (int)mPaceValue);
+                    }
+                    break;
+                case DISTANCE_MSG:
+                    mDistanceValue = ((int)msg.arg1)/1000f;
+                    if (mDistanceValue <= 0) { 
+                        mDistanceValueView.setText("0");
+                    }
+                    else {
+                        mDistanceValueView.setText(
+                                ("" + (mDistanceValue + 0.000001f)).substring(0, 5)
+                        );
+                    }
+                    break;
+                case SPEED_MSG:
+                    mSpeedValue = ((int)msg.arg1)/1000f;
+                    if (mSpeedValue <= 0) { 
+                        mSpeedValueView.setText("0");
+                    }
+                    else {
+                        mSpeedValueView.setText(
+                                ("" + (mSpeedValue + 0.000001f)).substring(0, 4)
+                        );
+                    }
+                    break;
+                case CALORIES_MSG:
+                    mCaloriesValue = msg.arg1;
+                    if (mCaloriesValue <= 0) { 
+                        mCaloriesValueView.setText("0");
+                    }
+                    else {
+                        mCaloriesValueView.setText("" + (int)mCaloriesValue);
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+        
+    };
+    
+
+}
